50286d172f3e4ee67220e637baa7ce3dbe213e8f
davidauza.engineer <davidauza01@gmail.com>
2020-05-18 15:51:06 -0500

    Create proposal-new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { DocumentsComponent } from './documents/documents.component';
 import { ProposalListComponent } from './proposals/proposal-list/proposal-list.component';
+import { ProposalNewComponent } from './proposals/proposal-new/proposal-new.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
     DocumentsComponent,
-    ProposalListComponent
+    ProposalListComponent,
+    ProposalNewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
