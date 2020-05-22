c95ba7768a7a571a2ce8d71e90d3360ad8845680
davidauza.engineer <davidauza01@gmail.com>
2020-05-22 16:18:10 -0500

    Add goToShow function to proposal-list component

--- src/app/proposals/proposal-list/proposal-list.component.ts
+++ src/app/proposals/proposal-list/proposal-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { timer } from 'rxjs';
 import { Proposal } from '../proposal';
 import { ProposalService } from '../proposal.service';
@@ -13,7 +14,8 @@ export class ProposalListComponent implements OnInit {
   proposals: Proposal[];
 
   constructor(
-    private proposalService: ProposalService
+    private proposalService: ProposalService,
+    private router: Router
   ) { }
 
   ngOnInit() {
@@ -26,5 +28,10 @@ export class ProposalListComponent implements OnInit {
       .subscribe(
         proposals => this.proposals = proposals
       );
+  }
+
+  goToShow(proposal: Proposal): void {
+    const link = ['/proposals', proposal.id];
+    this.router.navigate(link);
   }
 }
